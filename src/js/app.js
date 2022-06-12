b06f8be5132986a8ab419b65c1a779fa1e0d85bb
HadiOryaniPr <HadiOryaniHadiOryani>
2022-06-12 19:15:18 +0430

    initial commit

---
+++ src/js/app.js
@@ -0,0 +1,8 @@
+"use strict";
+
+const hamburgurMenu = document.getElementById("hamburger");
+const mobileMenu = document.getElementById("mobile-menu");
+
+hamburgurMenu.addEventListener("click", () => {
+  mobileMenu.classList.toggle("hidden");
+});
