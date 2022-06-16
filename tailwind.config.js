fb6ba6829ade8d1937fd71bcafed7452f774f4b6
HadiOryaniPr <Hadioryanipr@gmail.com>
2022-06-16 19:05:51 +0430

    add footer

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,16 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {
-      colors : {
-        "primary" : "#DB2763"
+      colors: {
+        primary: "#DB2763",
       },
       screens: {
-        'sm2': {'min': '768px', 'max': '1056px'},
-    },
-    height: {
-      '94': '23.3rem',
-    }
+        sm2: { min: "768px", max: "1056px" },
+      },
+      height: {
+        94: "23.3rem",
+      },
     },
   },
   plugins: [],
-}
+};
