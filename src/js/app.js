ed0d5e67f006af6dda16096b075bad63702cae60
Hadi Oryani <83688429+HadiOryaniPr@users.noreply.github.com>
2022-12-18 18:32:27 +0330

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -2,6 +2,7 @@
 
 const hamburgurMenu = document.getElementById("hamburger");
 const mobileMenu = document.getElementById("mobile-menu");
+
 hamburgurMenu.addEventListener("click", () => {
   mobileMenu.classList.toggle("hidden");
 });
